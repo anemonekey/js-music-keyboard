b6d5a2d45d66a65c5f5a62698571c43b26de57c4
kee nam <nam.keeyeon@gmail.com>
2017-11-20 14:58:39 -0800

    Implmented click and keydown event handlers

--- noise.js
+++ noise.js
@@ -1,3 +1,27 @@
-$(document).ready( function() {
-  // your code here
+$(document).ready(function() {
+  const notes = {
+    65: 'a',
+    66: 'b',
+    67: 'c',
+    68: 'd',
+    69: 'e',
+    70: 'f',
+    71: 'g',
+  }
+
+  $('.instrument').on('click', 'button', function(event) {
+    $(this).click(function(event) {
+      note = $(this).html();
+      let noteId = "#" + note + "Audio";
+      $(noteId).get(0).play();
+    });
+  });
+
+  $('body').keydown(function(event) {
+    if (notes[event.which]) {
+      note = notes[event.which];
+      let noteId = "#" + note + "Audio";
+      $(noteId).get(0).play();
+    }
+  });
 });
